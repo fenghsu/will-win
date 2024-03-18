fcc353d4143c868789c5b5e73b696a0779664a2e
Feng <hsufengs@gmail.com>
2024-03-18 14:50:19 +0800

    Delete unused function

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
+
+// third party
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+//project imports
 import App from './App';
-import { store } from './store/index' 
+import { store } from './store/index';
+import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,3 +19,7 @@ root.render(
   </Provider>
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
