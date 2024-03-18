f3804dc63e069f98d0a78cc737b53e054f50dfff
Feng <hsufengs@gmail.com>
2024-03-18 14:49:53 +0800

    Delete unused function

--- src/routes/MainRoutes.js
+++ src/routes/MainRoutes.js
@@ -3,7 +3,6 @@ import { lazy } from 'react';
 // project imports
 import MainLayout from '../views/layout/index';
 import Loadable from '../ui-component/Loadable';
-//import AuthGuard from 'utils/route-guard/AuthGuard';
 
 // willWin routing
 const Material = Loadable(lazy(() => import('../views/basic-setting/material/MaterialPage')));
